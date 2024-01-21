e7f445dcbf3904d5bab50756fc4b96f156833524
Douglascrc <douglasbuscam@gmail.com>
2024-01-21 20:54:46 -0300

    Navbar in progress

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,29 +1,30 @@
-import React from "react";
-import "./Navbar.css";
+import "./Navbar.css"
 
 const Navbar = () => {
     return (
-        <nav className="sticky z-30 p-2 bg-black opacity-10 backdrop-blur-3xl">
-            <div>
-                <img src="../public/vite.svg" />
+        <nav className="bg-black/20 top-0 sticky p-2 backdrop-blur-3xl z-30">
+            <div className="max-w-screen-xl flex items-center justify-between pt-1">
+                <img className="w-12 h-16" src="../public/vite.svg" />
 
-                <ul>
+                <ul className="flex gap-3.5">
+
                     <li>
-                        <a href="">Home</a>   
+                        <a className="menu-item" href="">Home</a>   
                     </li>
                     <li>
-                        <a href="">Skills</a>
+                        <a className="menu-item" href="">Skills</a>
                     </li>
                     <li>
-                        <a href="">Projects Experience</a>
+                        <a className="menu-item" href="">Projects Experience</a>
                     </li>
                     <li>
-                        <a href="">Contact Me</a>
+                        <a className="menu-item" href="">Contact Me</a>
                     </li>
 
-                    <button className="" onClick={ () => {}}>
-                        Fale Comigo!
+                    <button className="contact-btn" onClick={ () => {}}>
+                        Call me!
                     </button>
+
                 </ul>
             </div>
         </nav>
