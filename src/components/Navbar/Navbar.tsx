8a000584a8b2c11cb3eafd69cfb9b44982a97445
Douglascrc <douglasbuscam@gmail.com>
2024-01-24 22:24:59 -0300

    refactor: add useState

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,11 +1,20 @@
-import { blue } from "@mui/material/colors";
+import { useState } from "react"
 import "./Navbar.css"
-
-import MenuIcon from '@mui/icons-material/Menu';
+import MenuIcon from '@mui/icons-material/Menu'
+import MobileNav from "../MobileNav/MobileNav"
 
 
 const Navbar = () => {
+    const [openMenu, setOpenMenu] = useState(false)
+
+    const toggleMenu = () => {
+        setOpenMenu(!openMenu)
+    }
+
     return (
+        <>
+        <MobileNav isOpen={openMenu} toggleMenu={toggleMenu}/>
+
         <nav className="bg-black/20 top-0 sticky p-2 backdrop-blur-3xl z-30">
             <div className=" nav-wrapper max-w-screen-xl flex items-center justify-between pt-1">
                 <img className="w-12 h-16" src="../public/vite.svg" />
@@ -31,9 +40,10 @@ const Navbar = () => {
 
                 </ul>
 
-                <button className="menu-btn bg-gradient-to-r from-violet-500 to-fuchsia-500 flex justify-center w-10 h-10 text-2xl rounded-md " onClick={() => {}}> 
+                <button className="menu-btn bg-gradient-to-r from-violet-500 to-fuchsia-500 flex justify-center w-10 h-10 text-2xl rounded-md " onClick={toggleMenu}> 
                 <span>
                 <MenuIcon fontSize="large"  /> 
+                {openMenu ? "close" : "menu"}
                 </span>
                    
                          
@@ -42,7 +52,8 @@ const Navbar = () => {
                 </button>
             </div>
         </nav>
+    </>
     );
 
 };
-export default Navbar;
+export default Navbar
