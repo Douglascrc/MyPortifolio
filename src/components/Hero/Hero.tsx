701efcceea5b3e81b29b8318be0f30542e5dfb21
Douglascrc <douglasbuscam@gmail.com>
2024-01-24 22:02:22 -0300

    feat: profile area

---
+++ src/components/Hero/Hero.tsx
@@ -0,0 +1,39 @@
+import './Hero.css'
+
+const Hero = () => {
+    return (
+        <section className='hero-content'>
+            <div>
+                <h1> Software Developer</h1>
+                <p>
+                    Studying technologies for development
+                </p>
+            </div>
+
+            <div className='hero-img'>
+                <div>
+                    <div className='tech-icon'>
+                        <img src="" alt="" />
+                    </div>
+                    <div>
+                        <img src="" alt="" />
+                    </div>
+                </div>
+
+                <div>
+                    <div>
+                        <img src="" alt="" />
+                    </div>
+                    <div>
+                        <img src="" alt="" />
+                    </div>
+                    <div>
+                        <img src="" alt="" />
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Hero
