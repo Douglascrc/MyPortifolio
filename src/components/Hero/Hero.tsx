ce67205d554ce1920911cb5c1239d76f267b1e2c
Douglascrc <douglasbuscam@gmail.com>
2024-02-15 22:28:20 -0300

    feat: add modificate Hero section

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -2,37 +2,36 @@ import './Hero.css'
 
 const Hero = () => {
     return (
-        <section className='flex relative items-center'>
-            <div className=' px-2 gap-2 flex-1'>
-                <h1 className='font-bold text-5xl mb-4'> Software Developer</h1>
-                <p className='text-base font-normal'>
+        <section className='hero-container flex relative items-center ml-8 '>
+            <div className='hero-content flex-1'>
+                <h1 className='font-bold text-8xl mb-4'> Software Developer</h1>
+                <p className='text-base text-center font-normal w-4/5'>
                     Studying technologies for development
                 </p>
             </div>
 
-            <div className='flex flex-1 gap-2 items-center mt-20 flex-col'>
-                <div className='flex gap-2 items-end'>
-                    <div className='w-20 items-center justify-center flex text-center rounded-xl'>
-                        <img src="../src/assets/react.svg" alt="" />
-                    </div>
-                    <div className='flex gap-2 items-end'>
-                        <img className='h-96 rounded-full' src="../src/assets/pfl3.png" alt="" />
-                    </div>
-                </div>
+            <div className='hero-img flex flex-1 flex-col items-center gap-8 mt-20'>
 
                 <div className='flex gap-2 items-end'>
-                    <div className='w-20 flex'>
-                       <img className='' src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nodejs/nodejs-original-wordmark.svg" alt='' />
-          
+                    <div className='tech-icon w-24 h-24 items-center justify-center flex text-center border border-solid border-[#6852ba] rounded-xl' >                      
+                        <img className='w-16 transition-all ease-in duration-300 hover:-translate-y-2' src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg" alt="" />
                     </div>
-                    <div>
-                        <img className='w-20 items-center justify-center flex text-center rounded-xl' src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/css3/css3-original.svg" alt="" />
+                    <div className='tech-icon w-24 h-24 items-center justify-center flex text-center border border-solid border-[#6852ba] rounded-xl'>
+                       <img className='w-16 transition-all ease-in duration-300 hover:-translate-y-2' src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nodejs/nodejs-original-wordmark.svg" alt='' />
                     </div>
-                    <div>
-                        <img className='w-20 items-center justify-center flex text-center rounded-xl' src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg" alt="" />
+                    <img className='w-96 rounded-full transition-all ease-in duration-300 hover:-translate-y-2' src="../src/assets/pfl3.png" alt="" />
+
+                </div>
+
+                <div className='flex gap-6 items-end justify-center'>
+                    <div  className='tech-icon w-24 h-24 items-center justify-center flex text-center border border-solid border-[#6852ba] rounded-xl'>
+                        <img className='w-16 transition-all ease-in duration-300 hover:-translate-y-2' src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/css3/css3-original.svg" alt="" />
                     </div>
-                    <div>
-                        <img className='w-20 items-center justify-center flex text-center rounded-xl' src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original.svg" alt="" />
+                    <div  className='tech-icon w-24 h-24 items-center justify-center flex text-center border border-solid border-[#6852ba] rounded-xl'>
+                        <img className='w-16 transition-all ease-in duration-300 hover:-translate-y-2' src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg" alt="" />
+                    </div>
+                    <div  className='tech-icon w-24 h-24 items-center justify-center flex text-center border border-solid border-[#6852ba] rounded-xl'>
+                        <img className='w-16 transition-all ease-in duration-300 hover:-translate-y-2' src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original.svg" alt="" />
                     </div>
                 </div>
             </div>
