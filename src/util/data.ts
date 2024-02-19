6d27cb030aaa8ff172162808898f3f02faf56ce8
Douglascrc <douglasbuscam@gmail.com>
2024-02-18 21:17:03 -0300

    feat: add skills data

---
+++ src/util/data.ts
@@ -0,0 +1,38 @@
+export const SKILLS  = [
+    {
+        title: "Frontend",
+        icon: "src/assets/frontend.svg",
+        skills: [
+            {skill:"HTML5", percentagem: "90%"},
+            {skill:"CSS3", percentagem:"80%"},
+            {skill:"JavaScript", percentagem:"80%"},
+            {skill:"ReactJS", percentagem:"60%"},
+        ],
+    },
+    {
+        title:"Backend",
+        icon: "src/assets/backend.svg",
+        skills: [
+            {skill:"NodeJS", percentagem:"70%"},
+            {skill:"ExpressJS", percentagem:"60%"},
+        ],
+    },
+    {
+        title: "Tools",
+        icon: "src/assets/tools.svg",
+        skills: [
+            {skill:"Git & GitHub", percentagem:"50%"},
+            {skill:"VSCODE", percentagem:"60%"},
+            {skill:"Docker", percentagem:"60%"}
+        ],
+    },
+    {
+        title: "Soft Skills",
+        icon: "src/assets/softskills.svg",
+        skills: [
+            {skill:"Problem-solved", percentagem:"100%"},
+            {skill:"Collaboration", percentagem:"100%"},
+            {skill:"Communication", percentagem:"80%"},
+        ],
+    },
+]
