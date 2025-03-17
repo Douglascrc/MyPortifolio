9442088b96937b9ad400fe6de10dcf59260d1218
Douglascrc <douglasbuscam@gmail.com>
2025-03-17 19:52:21 -0300

    feat: enhance mobile nav

--- src/components/Navbar/MobileNav/MobileNav.tsx
+++ src/components/Navbar/MobileNav/MobileNav.tsx
@@ -1,39 +1,55 @@
-import "./MobileNav.css"
-import Vite from "../../../../public/vite.svg"
+import "./MobileNav.css";
+import Dev from "../../../assets/command.svg";
 
-const MobileNav = ({isOpen, toggleMenu}: { isOpen: boolean, toggleMenu: () => void }) => {
-    return (
-        <div
-            className={`mobile-menu ${isOpen ? "active": ""}`} onClick={toggleMenu}>
-            
-            <div className="h-screen w-3/5 p-8 bg-black">
-                <img className="mb-4" src={Vite} alt="logo vite" />
-
-                <ul className="flex flex-col gap-4 -ml-4">
-
-                    <li>
-                        <a className="menu-item" href="">Home</a>   
-                    </li>
-                    <li>
-                        <a className="menu-item" href="">Skills</a>
-                    </li>
-                    <li>
-                        <a className="menu-item" href="">Projects Experience</a>
-                    </li>
-                    <li>
-                        <a className="menu-item" href="">Contact Me</a>
-                    </li>
-
-                    <button className="contact-btn" onClick={ () => {}}>
-                        Call me!
-                    </button>
-
-                </ul>
-            </div>
+const MobileNav = ({
+  isOpen,
+  toggleMenu,
+}: {
+  isOpen: boolean;
+  toggleMenu: () => void;
+}) => {
+  return (
+    <div
+      className={`mobile-menu ${isOpen ? "active" : ""}`}
+      onClick={toggleMenu}
+    >
+      <div className="h-screen w-3/5 p-8 bg-gradient-to-b from-black to-[#130f2a]">
+        <div className="flex items-center border-b border-[#6751b9] pb-4 mb-6">
+          <img className="w-12 h-12" src={Dev} alt="logo" />
+          <h2 className="ml-2 text-xl font-semibold bg-gradient-to-r from-[#a892fe] to-[#8064e8] bg-clip-text text-transparent">
+            Portfolio
+          </h2>
         </div>
 
+        <ul className="flex flex-col gap-4 -ml-4">
+          <li>
+            <a className="menu-item" href="">
+              Home
+            </a>
+          </li>
+          <li>
+            <a className="menu-item" href="">
+              Skills
+            </a>
+          </li>
+          <li>
+            <a className="menu-item" href="">
+              Projects Experience
+            </a>
+          </li>
+          <li>
+            <a className="menu-item" href="">
+              Contact Me
+            </a>
+          </li>
 
-    )
-}
+          <button className="contact-btn" onClick={() => {}}>
+            Call me!
+          </button>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
-export default MobileNav
+export default MobileNav;
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,55 +1,67 @@
-import { useState } from "react"
-import "./Navbar.css"
-import MenuIcon from '@mui/icons-material/Menu'
-import MobileNav from "./MobileNav/MobileNav"
-import CloseIcon from '@mui/icons-material/Close';
-import Vite from "../../../public/vite.svg"
- 
+import { useState } from "react";
+import "./Navbar.css";
+import MenuIcon from "@mui/icons-material/Menu";
+import MobileNav from "./MobileNav/MobileNav";
+import CloseIcon from "@mui/icons-material/Close";
+import Dev from "../../assets/command.svg";
+
 const Navbar = () => {
-    const [openMenu, setOpenMenu] = useState(false)
+  const [openMenu, setOpenMenu] = useState(false);
 
-    const toggleMenu = () => {
-        setOpenMenu(!openMenu)
-    }
+  const toggleMenu = () => {
+    setOpenMenu(!openMenu);
+  };
 
-    return (
-        <>
-        <MobileNav isOpen={openMenu} toggleMenu={toggleMenu}/>
+  return (
+    <>
+      <MobileNav isOpen={openMenu} toggleMenu={toggleMenu} />
 
-        <nav className="bg-black/20 top-0 sticky p-2 backdrop-blur-3xl z-30">
-            <div className=" nav-wrapper flex items-center justify-between pt-1 mx-3">
-                <img className="w-12 h-16" src={Vite}/>
+      <nav className="bg-black/20 top-0 sticky p-2 backdrop-blur-3xl z-30">
+        <div className=" nav-wrapper flex items-center justify-between pt-1 mx-3">
+          <img className="w-16 h-16" src={Dev} />
 
-                <ul className="flex gap-4">
+          <ul className="flex gap-4">
+            <li>
+              <a className="menu-item" href="#home">
+                Home
+              </a>
+            </li>
+            <li>
+              <a className="menu-item" href="#skills">
+                Habilidades
+              </a>
+            </li>
+            <li>
+              <a className="menu-item" href="#experience">
+                Experiência
+              </a>
+            </li>
+            <li>
+              <a className="menu-item" href="#contactme">
+                Entre em contato
+              </a>
+            </li>
 
-                    <li>
-                        <a className="menu-item" href="#home">Home</a>   
-                    </li>
-                    <li>
-                        <a className="menu-item" href="#skills">Habilidades</a>
-                    </li>
-                    <li>
-                        <a className="menu-item" href="#experience">Experiência</a>
-                    </li>
-                    <li>
-                        <a className="menu-item" href="#contactme">Entre em contato</a>
-                    </li>
+            <button className="contact-btn" onClick={() => {}}>
+              Me Chame!
+            </button>
+          </ul>
 
-                    <button className="contact-btn" onClick={ () => {}}>
-                        Me Chame!
-                    </button>
-
-                </ul>
-
-                <button className="menu-btn bg-gradient-to-r from-violet-500 to-fuchsia-500 flex justify-center w-10 h-10 text-2xl rounded-md ease-in duration-300 md:hidden" onClick={toggleMenu}> 
-                <span>
-                {openMenu ? <CloseIcon fontSize="large"/> : <MenuIcon fontSize="large"/> }
-                </span>                        
-                </button>
-            </div>
-        </nav>
+          <button
+            className="menu-btn bg-gradient-to-r from-violet-500 to-fuchsia-500 flex justify-center w-10 h-10 text-2xl rounded-md ease-in duration-300 md:hidden"
+            onClick={toggleMenu}
+          >
+            <span>
+              {openMenu ? (
+                <CloseIcon fontSize="large" />
+              ) : (
+                <MenuIcon fontSize="large" />
+              )}
+            </span>
+          </button>
+        </div>
+      </nav>
     </>
-    );
-
+  );
 };
-export default Navbar
+export default Navbar;
